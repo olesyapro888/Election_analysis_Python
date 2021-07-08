5b04dcb8321e8ffcfc510018e20b320a232a7db0
Olesya Irkhina <itekkie888@gmail.com>
2021-07-08 16:42:03 -0400

    adds files

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+# The data need to retrieve.
+#1. Total number of votes cast
+#2. A complete list of candidates who received votes
+#3. Total number of votes each candidate received
+#4. Percentage of votes each candidate won
+#5. The winner of the election based on popular vote
