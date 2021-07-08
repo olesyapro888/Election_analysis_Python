51de7716c627fbc7d0fe5f8271096179a854e1f7
Olesya Irkhina <itekkie888@gmail.com>
2021-07-08 06:11:58 -0400

    add files

---
+++ python_practice.py
@@ -0,0 +1,8 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+#if counties[1] == 'Denver':
+ # print(counties[1])
+
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
