c67a41ddfcdd8c9e38b798e00b6f13154b05ab53
Olesya Irkhina <itekkie888@gmail.com>
2021-07-11 14:52:49 -0400

    update pypoll

--- PyPoll.py
+++ PyPoll.py
@@ -33,18 +33,19 @@ winning_count = 0
 winning_percentage = 0
 
 # Open the election results and read the file.
-  #election_data = open(file_to_load, 'r')
+#election_data = open(file_to_load, 'r')
 with open(file_to_load) as election_data:
 # To do: perform analysis.
 #print(election_data)
 # Close the file. #election_data.close()
 
-# Read the file object with the reader function.
+  # Read the file object with the reader function.
   file_reader = csv.reader(election_data)
   
   # Read (and print) the header row.
   headers = next(file_reader)
-  #print(headers)
+  #row00 = next(file_reader)
+  #print(row00)
 
   #Print each row in the CSV file.
   for row in file_reader:
@@ -60,24 +61,22 @@ with open(file_to_load) as election_data:
       # Add it to the list of candidates.
       candidate_options.append(candidate_name)
     
-    # Setting each candidate's vote count to zero by using format "dictionary_name[key]" to count a value 
+    # To count a value by setting each candidate's vote count to zero in IF statement
       candidate_votes[candidate_name] = 0
   
-  # Add a vote to that candidate's count
+    # Add a vote to that candidate's count for the dictionary
     candidate_votes[candidate_name] +=1 
 
-  # Save the results to our text file.
-
-
-  # Using the open() function with the "w" mode we will write data to the file.
-  #outfile = open(file_to_save, "w")
-  # Using the with statement open the file as a text file.
+# Using the open() function with the "w" mode we will write data to the file.
+#outfile = open(file_to_save, "w")
+# Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
 # Write some data to the file.
 #txt_file.write("Counties in the Election\n------------------------\nArapahoe\nDenver\nJefferson")
 # Close the file
 #outfile.close()
-# Print the final vote count to the terminal.
+
+  # Print the final vote count to the terminal.
   election_results = (
     f"\nElection Results\n"
     f"-------------------------\n"
@@ -87,7 +86,7 @@ with open(file_to_save, "w") as txt_file:
   # # After printing the final vote count to the terminal save the final vote count to the text file.
   txt_file.write(election_results)
 
-# Determine the percentage of votes for each candidate by looping through the counts. # Iterate through the candidate list. 
+  # Determine the percentage of votes for each candidate by looping through the counts. # Iterate through the candidate list. 
   for candidate_name in candidate_votes:
     # Retrieve vote count of a candidate by assignign a variable for the value of key to calculate %
     votes = candidate_votes[candidate_name]
@@ -118,6 +117,7 @@ with open(file_to_save, "w") as txt_file:
       f"-------------------------\n")
   print(winning_candidate_summary)
   
+  #print(candidate_votes.keys())
   # 3. Print the total votes.
   #print(total_votes)
   # Print the candidate list.
