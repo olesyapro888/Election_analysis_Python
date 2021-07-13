ef281ce7c8d5823afef0def1c7cda66b36765131
Olesya Irkhina <itekkie888@gmail.com>
2021-07-13 06:56:23 -0400

    update python_practice

--- Others/grade_ifelif.py
+++ Others/grade_ifelif.py
@@ -12,3 +12,22 @@ elif score >= 60:
     print('Your grade is a D.')
 else:
     print('Your grade is an F.')
+
+
+#What is the score?
+# score = int(input("What is your test score? "))
+
+# Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# else:
+#     if score >= 80:
+#         print('Your grade is a B.')
+#     else:
+#         if score >= 70:
+#             print('Your grade is a C.')
+#         else:
+#             if score >= 60:
+#                 print('Your grade is a D.')
+#             else:
+#                 print('Your grade is an F.')
